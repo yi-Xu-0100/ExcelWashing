c0d80cbb9e8e4aef5b899af756f7e894181a6c93
yi-xu-0100 <xjy123456120@163.com>
2017-08-10 12:45:43 +0800

    updata class and attr

--- MyPyBag/generalFunction.py
+++ MyPyBag/generalFunction.py
@@ -11,10 +11,10 @@ description:
 __author__ = 'yi_Xu'
 
 import os
-import functools
 import sys
 import shutil
 import pandas as pd
+import numpy as np
 from datetime import datetime
 
 #基类
@@ -99,6 +99,76 @@ class Batch(object):
         else:
             print("There is none of new data which need to pull request!")
 
-            
-            
-            
+#设置jsyb类，继承于Batch
+class Jsyb(Batch):
+    """
+        getName, batchProcessing, getNewData, updata 是通用函数，不予复写
+    """
+    #复写__init__函数，定义jsyb类的属性
+    def __init__(self, filesPath, csvPath, bakPath, fileName, columns): 
+        super().__init__(filesPath, csvPath, bakPath, fileName, columns) #初始化各个的值
+        df = self.batchProcessing() #调用批处理，获得所有xls文件的数据
+        self.backupData() #备份源csv文件
+        self.newData, self.data = self.getNewData(df) #获取与源csv对比后得到的新数据和合并新数据后的总数据
+        self.updata(self.newData) #更新csv文件
+        self.money = self.data['当月结存']
+    #复写dataWashing函数，定义表格处理方式
+    def dataWashing(self,name):
+        try: #获取固定数据
+            pName = os.path.join(self.filesPath, name)
+            jsyb = pd.read_excel(pName, '客户交易结算月报', header=None)
+            df1 = pd.DataFrame()
+            a1 = pd.Series(jsyb.iloc[4,7], index=jsyb.index[:1])
+            df1['交易月份'] = a1
+            a2 = pd.Series(jsyb.iloc[9,2], index=jsyb.index[:1])
+            df1['上月结存'] = a2
+            a3 = pd.Series(jsyb.iloc[10,2], index=jsyb.index[:1])
+            df1['当月存取合计'] = a3
+            a4 = pd.Series(jsyb.iloc[11,2], index=jsyb.index[:1])
+            df1['当月盈亏'] = a4
+            a5 = pd.Series(jsyb.iloc[12,2], index=jsyb.index[:1])
+            df1['当月总权利金'] = a5
+            a6 = pd.Series(jsyb.iloc[13,2], index=jsyb.index[:1])
+            df1['当月手续费'] = a6
+            a7 = pd.Series(jsyb.iloc[14,2], index=jsyb.index[:1])
+            df1['当月结存'] = a7
+            a8 = pd.Series(jsyb.iloc[15,2], index=jsyb.index[:1])
+            df1['浮动盈亏'] = a8
+            b1 = pd.Series(jsyb.iloc[9,7], index=jsyb.index[:1])
+            df1['客户权益'] = b1
+            b2 = pd.Series(jsyb.iloc[10,7], index=jsyb.index[:1])
+            df1['实有货币资金'] = b2
+            b3 = pd.Series(jsyb.iloc[11,7], index=jsyb.index[:1])
+            df1['非货币充抵金额'] = b3
+            b4 = pd.Series(jsyb.iloc[12,7], index=jsyb.index[:1])
+            df1['货币充抵金额'] = b4
+            b5 = pd.Series(jsyb.iloc[13,7], index=jsyb.index[:1])
+            if type(jsyb.iloc[13,7]) != type(1): #此处的冻结资金有异常数据为"--"，加以判断并设为NaN。
+                b5 = np.repeat(np.nan, len(b5))
+            df1['冻结资金'] = b5
+            b6 = pd.Series(jsyb.iloc[14,7], index=jsyb.index[:1])
+            df1['保证金占用'] = b6
+            b7 = pd.Series(jsyb.iloc[15,7], index=jsyb.index[:1])
+            df1['可用资金'] = b7
+            b8 = pd.Series(jsyb.iloc[16,7], index=jsyb.index[:1])
+            df1['风险度'] = b8
+            b9 = pd.Series(jsyb.iloc[17,7], index=jsyb.index[:1])
+            df1['追加保证金'] = b9
+            crjmx = jsyb.iloc[21:-3,:]
+            if len(crjmx.index) != 0: #以下数据在部分表格中没有，加以判断以选择是否忽略处理。
+                crjmx.columns = ['交易日期', 1, '入金', 3, '出金', 5, '方式', 7, '摘要']
+                crjmx = crjmx[['交易日期', '入金', '出金', '方式', '摘要']]
+                crjmxb = crjmx.copy()
+                a = pd.Series(jsyb.iloc[4,7], index=crjmxb.index)
+                crjmxb['交易月份'] = a
+                crjmxb.index = range(len(crjmxb.index))
+                df = pd.merge(df1, crjmxb, how='outer', on='交易月份', left_index=True, right_index=True)
+            else: #如果上述数据没有，只需要整理基本数据。
+                df = df1
+        except Exception as e: #如果有异常，捕捉并报错。
+            print("Error in line: %s ，file name : %s" % (sys._getframe().f_lineno + 1, './MyPyBag/jsybFunction.py')) #显示报错的在文件的多少行和文件名。
+            print(pName + "==>'客户交易结算月报' ***读取*** 异常，请检查！") #提示报错信息。
+            print("Error:", e) #提示系统报错内容。
+        else:
+            print("%s is successfully read!" % name) #如果没有错误，显示读取成功。
+            return(df)
--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import pandas as pd
 import numpy as np
 from pandas import Series,DataFrame #这个虽然导入这两个库，但是并没有使用，延续使用了pd库的格式。
 from MyPyBag import generalFunction as gf
-from MyPyBag import jsybFunction as jf
 #导入完成
 
 if __name__ == '__main__':
@@ -28,12 +27,8 @@ if __name__ == '__main__':
         columns = ['交易日期','交易月份','上月结存','当月存取合计','当月盈亏','当月总权利金','当月手续费','当月结存','浮动盈亏','客户权益','实有货币资金','非货币充抵金额','货币充抵金额','冻结资金','保证金占用','可用资金','风险度','追加保证金','入金','出金','方式','摘要'] #设置列的排列应该与csv表格一致
         csvPath = './csv'#input("请输入 'jsyb.csv' 表格位置：")
         bakPath = './bak'#input("请输入 'jsyb_bak.csv'表格位置：") #保存方式，文件名后添加时间戳，仅保存两个，每次删掉时间更早的一份。
-        jsyb = jf.Jsyb(filesPath, csvPath, bakPath, 'jsyb.csv', columns) #初始化一个jsyb类，建立实例
-        df = jsyb.batchProcessing() #调用批处理，获得所有xls文件的数据
-        jsyb.backupData() #备份源csv文件
-        newData = jsyb.getNewData(df)[0] #获取与源csv对比后得到的新数据
-        jsyb.updata(newData) #更新csv文件
-        data = jsyb.getNewData(df)[1] #获取合并新数据后的总数据
+        jsyb = gf.Jsyb(filesPath, csvPath, bakPath, 'jsyb.csv', columns) #初始化一个jsyb类，建立实例
+        print(jsyb.money)
     except Exception as e:
             print("Error in line: %s , file name : %s" % (sys._getframe().f_lineno + 1, './main.py')) #输出行号
             print("Error:", e)
